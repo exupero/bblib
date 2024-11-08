5056575e7d29f445f6b20bbb1e8eaa0b42f46ef5
Eric Shull <eric@exupero.org>
2024-11-08 07:31:57 -0500

    Rewrite pull.clj to use intermediate representation

--- pull.clj
+++ pull.clj
@@ -1,58 +1,108 @@
-(ns pull)
+(ns pull
+  (:require clojure.test
+            [babashka.deps :as deps]))
+(deps/add-deps '{:deps {io.github.matthewdowney/rich-comment-tests {:mvn/version "v1.0.3"}}})
+(require '[com.mjdowney.rich-comment-tests :as rct])
 
-(declare crawl)
-
-(defn pluck [o fields fetch]
-  (into {}
-        (map (fn [field]
-               (cond
-                 (or (keyword? field)
-                     (string? field))
-                 , [field (get o field)]
-                 (map? field)
-                 , (let [[k fields] (first field)]
-                     (cond
-                       (keyword? k)
-                       , (let [o' (if (namespace k)
-                                    (fetch [k o])
-                                    (get o k))]
-                           [k (crawl o' fields fetch)])
-                       (vector? k)
-                       , (let [[q & params] k
-                               filt (into [q o] params)]
-                           [q (crawl (fetch filt) fields fetch)])))
-                 :else
-                 , (throw (Exception. (str "don't know how to get field " (pr-str field) " from " (pr-str o)))))))
-        fields))
-
-(defn crawl [o fields fetch]
+(defn sub [field]
   (cond
-    (nil? o)
-    , o
-    (map? o)
-    , (if (= '[*] fields)
-        o
-        (pluck o fields fetch))
-    (sequential? o)
-    , (map #(crawl % fields fetch) o)
+    (and (keyword? field)
+         (not (namespace field)))
+    , {:key field}
+    (map? field)
+    , (let [[k vs] (first field)]
+        (if (vector? k)
+          {:fetch k :sub (map sub vs)}
+          {:key k :sub (map sub vs)}))
     :else
-    , (pluck o fields fetch)))
+    , nil))
 
-(defn pull [q fetch]
-  (let [[filt fields] (first q)]
-    (crawl (fetch filt) fields fetch)))
+(defn spec [q]
+  (let [[query fields] (first q)]
+    (cond-> {:fetch query}
+      (and (seq fields)
+           (not= '[*] fields))
+      , (assoc :sub (map sub fields)))))
+
+^:rct/test
+(comment
+  (spec {[:a ""] []})
+  ; =>
+  {:fetch [:a ""]}
+
+  (spec {[:a ""] ['*]})
+  ; =>
+  {:fetch [:a ""]}
+
+  (spec {[:a ""] [:name]})
+  ; =>
+  {:fetch [:a ""]
+   :sub [{:key :name}]}
+
+  (spec {[:a ""] [:name {:friends [:name]}]})
+  ; =>
+  {:fetch [:a ""]
+   :sub [{:key :name}
+         {:key :friends
+          :sub [{:key :name}]}]}
+
+  (spec {[:a ""] [{[:friends] [:name]}]})
+  ; =>
+  {:fetch [:a ""]
+   :sub [{:fetch [:friends]
+          :sub [{:key :name}]}]}
+
+  (spec {[:a ""] [{[:friends] [:name {[:status] [:mood]}]}]})
+  ; =>
+  {:fetch [:a ""]
+   :sub [{:fetch [:friends]
+          :sub [{:key :name}
+                {:fetch [:status]
+                 :sub [{:key :mood}]}]}]})
+
+(defn get-subs [o sub f]
+  (cond
+    (map? o)
+    , (into {}
+            (comp
+              (map (fn [s]
+                     (let [{k :key :keys [fetch sub]} s]
+                       (cond
+                         k
+                         , [k (get o k)]
+                         fetch
+                         , (let [[k & args] fetch
+                                 o (apply f k o args)]
+                             [(first fetch) (get-subs o sub f)]))))))
+            sub)
+    (sequential? o)
+    , (map #(get-subs % sub f) o)
+    :else
+    , (throw (Exception. (str "don't know how to get subs for " (pr-str o))))))
+
+(defn pull [q f]
+  (let [{:keys [fetch sub]} (spec q)]
+    (get-subs (apply f fetch) sub f)))
+
+^:rct/test
+(comment
+  (pull {[:person "alice"] [:name {[:friends] [:name {[:status] [:mood]}]}]}
+        (fn [& args]
+          (condp = args
+            [:person "alice"]
+            , {:id "alice" :name "Alice"}
+            [:friends {:id "alice" :name "Alice"}]
+            , [{:id "bob" :name "Bob"}
+               {:id "carl" :name "Carl"}]
+            [:status {:id "bob" :name "Bob"}]
+            , {:mood :dour}
+            [:status {:id "carl" :name "Carl"}]
+            , {:mood :happy})))
+  ; =>
+  {:name "Alice"
+   :friends [{:name "Bob" :status {:mood :dour}}
+             {:name "Carl" :status {:mood :happy}}]})
 
 (comment
-  (defmulti fetch first)
-  (defmethod fetch :item/id [_]
-    {:id "a4340bef-ec27-49a5-a362-32f9cd699381"
-     :name "Alice"
-     :friends [{:id "fe788a57-9af1-4402-aca8-0dee3362d92d" :name "Bob"}
-               {:id "057c4a19-0101-4fcc-a87f-574ebd8d13cc" :name "Carl"}]})
-  (defmethod fetch :friend/status [[_ {:keys [id name]}]]
-    {:id id :name name :status :sad})
-
-  (pull '{[:item/id "..."] [:name
-                            {:friends [:name
-                                       {:friend/status [:id :status]}]}]}
-        fetch))
+  (binding [clojure.test/*test-out* *out*]
+    (rct/run-ns-tests! *ns*)))
