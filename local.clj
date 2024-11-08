eb6563506346a68f4e6e9837acd1bfaee2ac0d3e
Eric Shull <eric@exupero.org>
2024-11-08 07:31:57 -0500

    Fix path parts not being joined by /

--- local.clj
+++ local.clj
@@ -1,5 +1,6 @@
 (ns local
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
 (defn home [& path-parts]
-  (io/file (apply str (System/getenv "HOME") "/" path-parts)))
+  (io/file (apply str (System/getenv "HOME") "/" (str/join "/" path-parts))))
