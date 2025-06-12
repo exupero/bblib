f9c6dd7b2bbe24d84b7a2a836fef1575e09de6b1
Eric Shull <eric@exupero.org>
2025-06-12 16:00:41 -0400

    Check in

--- obsidian.clj
+++ obsidian.clj
@@ -1,8 +1,18 @@
 (ns obsidian
-  (:require text))
+  (:require [babashka.process :as p]
+            text))
+
+(defn open! [uri]
+  (p/shell "open" uri))
 
 (defn uri [kvs]
   (str "obsidian://new?" (text/format-query-params kvs)))
 
+(defn uri! [kvs]
+  (open! (uri kvs)))
+
 (defn advanced-uri [kvs]
   (str "obsidian://adv-uri?" (text/format-query-params kvs)))
+
+(defn advanced-uri! [kvs]
+  (open! (advanced-uri kvs)))
