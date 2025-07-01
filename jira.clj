f7e785a8d04773a05d227378c0148a2ec4e2348f
Eric Shull <eric@exupero.org>
2025-07-01 17:10:31 -0400

    Check in

--- jira.clj
+++ jira.clj
@@ -8,7 +8,7 @@
 
 (def normalize-project-key (comp str/upper-case name))
 
-(defn as-jira-request [{:keys [path] :as params}]
+(defn configure-request [{:keys [path] :as params}]
   (-> params
       (dissoc :path)
       (assoc :url (str host path)
@@ -17,34 +17,34 @@
                        "Content-Type" "application/json"})))
 
 (defn search [query]
-  (as-jira-request
+  (configure-request
     {:path "/search"
      :method :get
      :query-params {:jql query}}))
 
 (defn ticket
   ([ticket-key]
-   (as-jira-request
+   (configure-request
      {:path (str "/issue/" (name ticket-key))
       :method :get}))
   ([project n]
-   (ticket (normalize-project-key (str project \- n)))))
+   (ticket (str (normalize-project-key project) \- n))))
 
 (defn components
   ([project] (components project {}))
   ([project params]
-   (as-jira-request
+   (configure-request
      {:path (str "/project/" (normalize-project-key project) "/component")
       :method :get
       :query-params params})))
 
 (defn project-statuses [project]
-  (as-jira-request
+  (configure-request
     {:path (str "/project/" (normalize-project-key project) "/statuses")
      :method :get}))
 
 (defn ticket-transitions [ticket]
-  (as-jira-request
+  (configure-request
     {:path (str "/issue/" ticket "/transitions")
      :method :get}))
 
@@ -53,14 +53,14 @@
                            http/request!
                            :transitions
                            (->> (filter (comp #{status} :name))))]
-    (-> (as-jira-request
+    (-> (configure-request
           {:path (str "/issue/" ticket "/transitions")
            :method :post
            :body {:transition {:id id}}})
         http/request!)))
 
 (defn ticket-update [ticket-id]
-  (as-jira-request
+  (configure-request
     {:path (str "/issue/" (normalize-project-key ticket-id))
      :method :put}))
 
@@ -71,7 +71,7 @@
   (assoc-in ticket [:body :fields :versions] [{:id version-id}]))
 
 (defn new-ticket [{:keys [project summary description issuetype components] :as fields}]
-  (as-jira-request
+  (configure-request
     {:path (str "/issue/")
      :method :post
      :body {:fields (merge fields
@@ -82,7 +82,7 @@
                             :components (map #(do {:name %}) components)})}}))
 
 (defn link [from-ticket to-ticket type]
-  (as-jira-request
+  (configure-request
     {:path (str "/issueLink")
      :method :post
      :body {:type {:name type}
@@ -95,6 +95,6 @@
     inwardIssue (conj (inwardIssue :key))))
 
 (defn versions [project]
-  (as-jira-request
+  (configure-request
     {:path (str "/project/" (normalize-project-key project) "/versions")
      :method :get}))
