3dbcd26cd339529195ba7419ff2b6a2cc6e2ceea
Eric Shull <eric@exupero.org>
2025-07-11 17:53:31 -0400

    Check in

--- markdown.clj
+++ markdown.clj
@@ -3,23 +3,33 @@
             [clj-yaml.core :as yaml]
             text))
 
+(defn frontmatter-text [s]
+  (when (= "---" (subs s 0 3))
+    (transduce
+      (comp
+        (drop 1)
+        (take-while (complement #{"---"}))
+        (interpose "\n"))
+      str (str/split-lines s))))
+
 (defn frontmatter [s]
   (when (= "---" (subs s 0 3))
-    (yaml/parse-string
-      (transduce
-        (comp
-          (drop 1)
-          (take-while (complement #{"---"}))
-          (interpose "\n"))
-        str (str/split-lines s)))))
+    (yaml/parse-string (frontmatter-text s))))
+
+(defn frontmatter-text+content [s]
+  (let [[head & tail] (-> s str/trim str/split-lines)]
+    (if (= head "---")
+      (let [[front back] (split-with (complement #{"---"}) tail)]
+        [(str/join "\n" front)
+         (str/join "\n" (drop 1 back))])
+      ["" (str/join "\n" (cons head tail))])))
 
 (defn frontmatter+content [s]
   (let [[head & tail] (-> s str/trim str/split-lines)]
     (if (= head "---")
-      (let [[front back] (split-with (complement #{"---"}) tail)]
-        [(yaml/parse-string (str/join "\n" front))
-         (str/join "\n" (drop 1 back))])
-      [{} (str/join "\n" tail)])))
+      (let [[fm-text content] (frontmatter-text+content s)]
+        [(yaml/parse-string fm-text) content])
+      [{} (str/join "\n" (cons head tail))])))
 
 (defn remove-frontmatter [s]
   (if (= "---" (subs s 0 3))
@@ -32,10 +42,20 @@
       str (str/split-lines s))
     s))
 
+(defn update-frontmatter-text [file f & args]
+  (text/update-file file
+    (fn [content]
+      (let [[fm-text content] (frontmatter-text+content content)]
+        (str "---\n"
+             (apply f fm-text args)
+             "\n---\n"
+             content)))))
+
 (defn update-frontmatter [file f & args]
-  (let [[fm content] (frontmatter+content (slurp file))]
-    (spit file
-          (str "---\n"
-               (yaml/generate-string (apply f fm args) :dumper-options {:flow-style :block})
-               "---\n"
-               content))))
+  (text/update-file file
+    (fn [content]
+      (let [[fm content] (frontmatter+content content)]
+        (str "---\n"
+             (yaml/generate-string (apply f fm args) :dumper-options {:flow-style :block})
+             "---\n"
+             content)))))
