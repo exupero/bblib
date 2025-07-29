09dd4b634649384eb9811cc04bfc10e891abcf77
Eric Shull <eric@exupero.org>
2025-07-29 17:43:25 -0400

    Check in

--- mongo.clj
+++ mongo.clj
@@ -1,9 +1,20 @@
 (ns mongo
+  (:refer-clojure :exclude [eval find])
   (:require [babashka.process :as p]
             [cheshire.core :as json]))
 
-(defn fetch-json [db script]
-  (-> (p/shell {:out :string} "mongosh" (name db) "--eval"
-               (str "JSON.stringify(" script ")"))
-      :out
+(defn eval [db script]
+  (-> (p/shell {:out :string} "mongosh" (name db) "--eval" script)
+      :out))
+
+(defn find [db coll query]
+  (-> (eval db (str "JSON.stringify(db." (name coll) ".find(" (json/generate-string query) ").toArray())"))
       json/parse-string))
+
+(defn update-one! [db coll doc]
+  (let [id (get doc :_id)
+        doc (dissoc doc :_id)]
+    (-> (eval db #p (str "JSON.stringify("
+                           "db." (name coll) ".updateOne({ _id: ObjectId('" id "') }, { $set: " (json/generate-string doc) " })"
+                         ")"))
+        json/parse-string)))
