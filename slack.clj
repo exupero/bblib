1396746f064d44bc8b27218777c8bf65d7524d8b
Eric Shull <eric@exupero.org>
2025-11-04 17:20:12 -0500

    Check in

--- slack.clj
+++ slack.clj
@@ -17,3 +17,25 @@
      :body {:profile {:status_text text
                       :status_emoji emoji
                       :status_expiration expiration}}}))
+
+(defn conversation-history
+  ([channel-id]
+   (conversation-history channel-id nil))
+  ([channel-id {:keys [oldest]}]
+   (configure-request
+     {:path "/conversations.history"
+      :method :get
+      :query-params {:channel channel-id
+                     :limit 100
+                     :oldest oldest}})))
+
+(defn post-message
+  ([channel-id text]
+   (post-message channel-id text nil))
+  ([channel-id text thread-ts]
+   (configure-request
+     {:path "/chat.postMessage"
+      :method :post
+      :body (cond-> {:channel channel-id
+                     :text text
+                     :thread_ts thread-ts})})))
